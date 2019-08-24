1a476fa8a8db03ac126a266f7818d71d6c7b767c
ZirixCZ <zirixcz@gmail.com>
2019-08-24 22:59:00 +0200

    design update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function inviteButton() {
     window.open("https://discordapp.com/oauth2/authorize?client_id=613806140077113364&permissions=8&scope=bot")
 }
+
+function textChange(){
+    document.getElementById('Feedback_p').innerHTML = "bakafeedback101@gmail.com";
+}

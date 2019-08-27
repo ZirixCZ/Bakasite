de11779002b50d2a2bc74798dafab58d272f4787
ZirixCZ <zirixcz@gmail.com>
2019-08-27 18:22:19 +0200

    design changes + discord icon

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ function inviteButton() {
     window.open("https://discordapp.com/oauth2/authorize?client_id=613806140077113364&permissions=8&scope=bot")
 }
 
+function ServerInvite() {
+  window.open("https://discord.gg/8c8Mwmy")
+}
+
 function textChange(){
-    document.getElementById('Feedback_p').innerHTML = "bakafeedback101@gmail.com";
+  document.getElementById('Feedback_p').innerHTML = "bakafeedback101@gmail.com";
+}
+
+function ClickFuncsFeedback(){
+    textChange();
 }
